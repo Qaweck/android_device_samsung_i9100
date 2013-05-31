9889e7e6b5664b401447e99ab9bd5a9ab7639766
Qaweck <qaweck@gmail.com>
2013-05-31 22:23:26 +0200

    Correct ril compilation Correct lunch
    Change-Id: I0914853f600f2d6fae3bd70292431aad78336565

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,5 +18,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_i9100p-eng
+add_lunch_combo cm_i9100p-userdebug
+
 
